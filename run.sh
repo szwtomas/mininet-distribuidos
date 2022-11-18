e8a2faaed5b4cd5bb22c707da9817fabceb57c8e
Tomas Szwarcberg <tomas.szwarcberg@innovid.com>
2022-11-18 15:10:17 -0300

    Creates run script for docker-compose

--- run.sh
+++ run.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 
-docker run -it --rm --privileged -e DISPLAY \
-             -v /tmp/.X11-unix:/tmp/.X11-unix \
-             -v /lib/modules:/lib/modules
+docker-compose run mininet
+
